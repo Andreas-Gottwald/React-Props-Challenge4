36bdbbb1f25dff2637023259237995481d00439a
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-27 14:38:17 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -2,5 +2,32 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("hahehihohu");
+  }
+  return (
+    <Button
+      text="SuperDuperButton"
+      backgroundColor="red"
+      color="khaki"
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button({ color, disabled, text, backgroundColor, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        height: "300px",
+        width: "300px",
+        backgroundColor: backgroundColor,
+        color: color,
+      }}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 }
